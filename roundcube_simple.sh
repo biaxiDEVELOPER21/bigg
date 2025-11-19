7117c3fd5c7ea08e8e0e5fae0a26f2e9cd3e8e70
biaxiDEVELOPER21 <biaxialhd@gmail.com>
2025-11-19 08:21:29 +0700

    Update roundcube_simple.sh

--- roundcube_simple.sh
+++ roundcube_simple.sh
@@ -51,36 +51,6 @@ echo "user1:password123" | chpasswd
 useradd -m user2 || true
 echo "user2:password123" | chpasswd
 
-echo "[6/10] Buat database roundcube..."
-mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS roundcube;
-CREATE USER IF NOT EXISTS 'roundcube'@'localhost' IDENTIFIED BY '$DBPASS';
-GRANT ALL PRIVILEGES ON roundcube.* TO 'roundcube'@'localhost';
-FLUSH PRIVILEGES;
-EOF
-
-echo "[7/10] Download Roundcube..."
-cd /var/www/html
-wget https://github.com/roundcube/roundcubemail/releases/download/1.6.7/roundcubemail-1.6.7-complete.tar.gz
-tar -xvf roundcubemail-1.6.7-complete.tar.gz
-mv roundcubemail-1.6.7 roundcube
-rm roundcubemail-1.6.7-complete.tar.gz
-cd roundcube
-
-echo "[8/10] Install Roundcube..."
-composer install --no-dev
-cp config/config.inc.php.sample config/config.inc.php
-
-sed -i "s|\$config\['db_dsnw'\].*|\$config['db_dsnw'] = 'mysql://roundcube:$DBPASS@localhost/roundcube';|" config/config.inc.php
-sed -i "s|// \$config\['default_host'\].*|\$config['default_host'] = 'localhost';|" config/config.inc.php
-sed -i "s|// \$config\['smtp_server'\].*|\$config['smtp_server'] = 'localhost';|" config/config.inc.php
-
-echo "[9/10] Import database schema..."
-mysql -u root roundcube < SQL/mysql.initial.sql
-
-echo "[10/10] Set permission & restart Apache..."
-chown -R www-data:www-data /var/www/html/roundcube
-systemctl restart apache2 postfix dovecot
 
 echo "=============================================="
 echo "INSTALL SELESAI (MODE LOCALHOST)"
